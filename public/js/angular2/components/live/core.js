4a427228f760bd6c3b1956881f691c1a96d827ba
Luke <luke@Lukes-iMac.local>
2016-05-09 22:48:35 +1200

    angular2 component templates

---
+++ public/js/angular2/components/live/core.js
@@ -0,0 +1,76 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var StreamsComponent = (function () {
+    function StreamsComponent() {
+    }
+    StreamsComponent = __decorate([
+        core_1.Component({
+            selector: 'streams',
+            templateUrl: 'js/angular2/templates/streams.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], StreamsComponent);
+    return StreamsComponent;
+}());
+exports.StreamsComponent = StreamsComponent;
+var StatusComponent = (function () {
+    function StatusComponent() {
+    }
+    StatusComponent = __decorate([
+        core_1.Component({
+            selector: 'status',
+            templateUrl: 'js/angular2/templates/status.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], StatusComponent);
+    return StatusComponent;
+}());
+exports.StatusComponent = StatusComponent;
+var UpdatesComponent = (function () {
+    function UpdatesComponent() {
+    }
+    UpdatesComponent = __decorate([
+        core_1.Component({
+            selector: 'updates',
+            templateUrl: 'js/angular2/templates/updates.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], UpdatesComponent);
+    return UpdatesComponent;
+}());
+exports.UpdatesComponent = UpdatesComponent;
+var ResourceComponent = (function () {
+    function ResourceComponent() {
+    }
+    ResourceComponent = __decorate([
+        core_1.Component({
+            selector: 'resource',
+            templateUrl: 'js/angular2/templates/resource.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ResourceComponent);
+    return ResourceComponent;
+}());
+exports.ResourceComponent = ResourceComponent;
+var UpdateComponent = (function () {
+    function UpdateComponent() {
+    }
+    UpdateComponent = __decorate([
+        core_1.Component({
+            selector: 'update',
+            templateUrl: 'js/angular2/templates/update.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], UpdateComponent);
+    return UpdateComponent;
+}());
+exports.UpdateComponent = UpdateComponent;
---
+++ public/js/angular2/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+require('rxjs/Rx');
+var live_component_1 = require('./components/live.component');
+platform_browser_dynamic_1.bootstrap(live_component_1.LiveComponent);
